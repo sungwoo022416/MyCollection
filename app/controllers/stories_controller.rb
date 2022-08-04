8e907ec3e9c1dbc909b35a56f8f4db3443deff2d
Sungwoo Kim <sungwoo.kim@yale.edu>
2022-08-03 22:34:10 -0700

    second draft

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -4,12 +4,17 @@ class StoriesController < ApplicationController
         render json: @stories, except: [:created_at, :updated_at]
     end
 
+    def create
+        @story = Story.create(story_params)
+        render json: @story, except: [:created_at, :updated_at]
+    end
+
     def update
         @story = Story.find(params[:id])
         @story.update(story_params)
         render json: @story, except: [:created_at, :updated_at]
     end
-
+    
 
     private
 
